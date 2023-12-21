df449d7e07be2010cb8e5f35dfa1900784567e2d
Ivana Milidragovic <ika.milidragovic@gmail.com>
2023-12-21 20:16:25 +0100

    rute projekta

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post("login", "App\Http\Controllers\UserController@login");
+Route::post("register", "App\Http\Controllers\UserController@register");
+
+//sanctum group
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::post("logout", "App\Http\Controllers\UserController@logout");
+    Route::resource("hitnosti", "App\Http\Controllers\HitnostController");
+    Route::get('usluge', 'App\Http\Controllers\UslugaController@index');
+    Route::get('usluge/{id}', 'App\Http\Controllers\UslugaController@show');
+    Route::post('usluge', 'App\Http\Controllers\UslugaController@store');
+    Route::put('usluge/{id}', 'App\Http\Controllers\UslugaController@update');
+    Route::delete('usluge/{id}', 'App\Http\Controllers\UslugaController@destroy');
+    Route::get('zahtevi', 'App\Http\Controllers\ZahtevController@index');
+    Route::get('zahtevi/{id}', 'App\Http\Controllers\ZahtevController@show');
+    Route::post('zahtevi', 'App\Http\Controllers\ZahtevController@store');
+    Route::put('zahtevi/{id}', 'App\Http\Controllers\ZahtevController@update');
+    Route::delete('zahtevi/{id}', 'App\Http\Controllers\ZahtevController@destroy');
+    Route::get('zahtevi/user/{id}', 'App\Http\Controllers\ZahtevController@findByUserId');
+    Route::get('zahtevi/usluge/{id}', 'App\Http\Controllers\ZahtevController@findByUslugaId');
+    Route::get('zahtevi/hitnosti/{id}', 'App\Http\Controllers\ZahtevController@findByHitnostId');
+    Route::get('paginacija', 'App\Http\Controllers\ZahtevController@paginate');
+});
