484e817b054e91414f9678c4a64b4b45e50684be
Andrea Tambur <andrea.tambur@gmail.com>
2023-12-21 20:10:43 +0100

    usluga i zahtev controller

---
+++ app/Http/Controllers/UslugaController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\UslugaResurs;
+use App\Models\Usluga;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UslugaController extends Controller
+{
+    //index
+
+    public function index()
+    {
+        $usluge = Usluga::all();
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitane usluge',
+            'podaci' => UslugaResurs::collection($usluge),
+        ], 200);
+    }
+
+    //show
+
+    public function show($id)
+    {
+        $usluga = Usluga::find($id);
+
+        if(!$usluga)
+        {
+            return response()->json([
+                'poruka' => 'Ne postoji usluga sa tim id',
+            ], 404);
+        }
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitana usluga',
+            'podaci' => new UslugaResurs($usluga),
+        ], 200);
+    }
+
+    //store
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nazivUsluge' => 'required',
+            'cena' => 'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'poruka' => 'Niste uneli sve podatke',
+                'podaci' => $validator->errors(),
+            ], 400);
+        }
+
+        $usluga = Usluga::create([
+            'nazivUsluge' => $request->nazivUsluge,
+            'cena' => $request->cena,
+        ]);
+
+        return response()->json([
+            'poruka' => 'Uspesno kreirana usluga',
+            'podaci' => new UslugaResurs($usluga),
+        ], 201);
+    }
+
+    //update
+
+    public function update(Request $request, $id)
+    {
+        $usluga = Usluga::find($id);
+
+        if(!$usluga)
+        {
+            return response()->json([
+                'poruka' => 'Ne postoji usluga sa tim id',
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'nazivUsluge' => 'required',
+            'cena' => 'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'poruka' => 'Niste uneli sve podatke',
+                'podaci' => $validator->errors(),
+            ], 400);
+        }
+
+        $usluga->update([
+            'nazivUsluge' => $request->nazivUsluge,
+            'cena' => $request->cena,
+        ]);
+
+        return response()->json([
+            'poruka' => 'Uspesno izmenjena usluga',
+            'podaci' => new UslugaResurs($usluga),
+        ], 200);
+    }
+
+    //destroy
+
+    public function destroy($id)
+    {
+        $usluga = Usluga::find($id);
+
+        if(!$usluga)
+        {
+            return response()->json([
+                'poruka' => 'Ne postoji usluga sa tim id',
+            ], 404);
+        }
+
+        $usluga->delete();
+
+        return response()->json([
+            'poruka' => 'Uspesno obrisana usluga',
+        ], 200);
+    }
+}
---
+++ app/Http/Controllers/ZahtevController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ZahtevResurs;
+use App\Models\Zahtev;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ZahtevController extends Controller
+{
+    //idnex
+
+    public function index()
+    {
+        $zahtevi = Zahtev::all();
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitani zahtevi',
+            'podaci' => ZahtevResurs::collection($zahtevi),
+        ], 200);
+    }
+
+    //show
+
+    public function show($id)
+    {
+        $zahtev = Zahtev::find($id);
+
+        if(!$zahtev)
+        {
+            return response()->json([
+                'poruka' => 'Ne postoji zahtev sa tim id',
+            ], 404);
+        }
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitan zahtev',
+            'podaci' => new ZahtevResurs($zahtev),
+        ], 200);
+    }
+
+    //store
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'nazivLjubimca' => 'required',
+            'vrstaLjubimca' => 'required',
+            'user_id' => 'required',
+            'hitnost_id' => 'required',
+            'usluga_id' => 'required',
+            'status' => 'required',
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'poruka' => 'Niste uneli sve podatke',
+                'podaci' => $validator->errors(),
+            ], 400);
+        }
+
+        $zahtev = Zahtev::create([
+            'nazivLjubimca' => $request->nazivLjubimca,
+            'vrstaLjubimca' => $request->vrstaLjubimca,
+            'user_id' => $request->user_id,
+            'hitnost_id' => $request->hitnost_id,
+            'usluga_id' => $request->usluga_id,
+            'status' => $request->status,
+        ]);
+
+        return response()->json([
+            'poruka' => 'Uspesno kreiran zahtev',
+            'podaci' => new ZahtevResurs($zahtev),
+        ], 201);
+    }
+
+    //update
+
+    public function update(Request $request, $id)
+    {
+        $zahtev = Zahtev::find($id);
+
+        if(!$zahtev)
+        {
+            return response()->json([
+                'poruka' => 'Ne postoji zahtev sa tim id',
+            ], 404);
+        }
+
+        $zahtev->update([
+            'nazivLjubima' => $request->nazivLjubima,
+            'vrstaLjubimca' => $request->vrstaLjubimca,
+            'user_id' => $request->user_id,
+            'hitnost_id' => $request->hitnost_id,
+            'usluga_id' => $request->usluga_id,
+            'status' => $request->status,
+        ]);
+
+        return response()->json([
+            'poruka' => 'Uspesno izmenjen zahtev',
+            'podaci' => new ZahtevResurs($zahtev),
+        ], 200);
+    }
+
+    //destroy
+
+    public function destroy($id)
+    {
+        $zahtev = Zahtev::find($id);
+
+        if(!$zahtev)
+        {
+            return response()->json([
+                'poruka' => 'Ne postoji zahtev sa tim id',
+            ], 404);
+        }
+
+        $zahtev->delete();
+
+        return response()->json([
+            'poruka' => 'Uspesno obrisan zahtev',
+        ], 200);
+    }
+
+    public function findByUserId(Request $request, $id)
+    {
+        $zahtevi = Zahtev::where('user_id', $id)->get();
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitani zahtevi',
+            'podaci' => ZahtevResurs::collection($zahtevi),
+        ], 200);
+    }
+
+    public function findByUslugaId(Request $request, $id)
+    {
+        $zahtevi = Zahtev::where('usluga_id', $id)->get();
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitani zahtevi',
+            'podaci' => ZahtevResurs::collection($zahtevi),
+        ], 200);
+    }
+
+    public function findByHitnostId(Request $request, $id)
+    {
+        $zahtevi = Zahtev::where('hitnost_id', $id)->get();
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitani zahtevi',
+            'podaci' => ZahtevResurs::collection($zahtevi),
+        ], 200);
+    }
+
+    public function paginate(Request $request)
+    {
+        $perPage = $request->query('per_page', 5);
+
+        $zahtevi = Zahtev::paginate($perPage);
+
+        return response()->json([
+            'poruka' => 'Uspesno ucitani zahtevi',
+            'podaci' => ZahtevResurs::collection($zahtevi),
+        ], 200);
+    }
+}
